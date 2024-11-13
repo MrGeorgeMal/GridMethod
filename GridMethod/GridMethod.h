f988dec913ecb2f3621853251091214414cb4245
Marshmlalik <mr.mageorge@yandex.ru>
2024-11-13 23:24:30 +0700

    Updated

--- GridMethod/GridMethod.h
+++ GridMethod/GridMethod.h
@@ -3,5 +3,19 @@
 
 class GridMethod
 {
+private:
+	StripStructure* stripStructure = new StripStructure();
+	double _minSizeX = 0.0;
+	double _minSizeY = 0.0;
+	double _divideMinSizeX = 1.0;
+	double _divideMinSizeX = 1.0;
+	double _dx = 1.0;
+	double _dy = 1.0;
+
+public:
+	GridMethod(StripStructure* stripStructure)
+	{
+		
+	}
 
 };
